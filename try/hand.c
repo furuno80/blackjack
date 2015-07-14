58db9d61c82db28ba7868a7bce4a076ef25b5067
James <dontwantto@gmail.com>
2015-07-14 11:09:29 -0400

    This is with recursive freehand

--- try/hand.c
+++ try/hand.c
@@ -1,1 +1,16 @@
 #include "hand.h"
+
+void free_hand(struct hand *hand)
+{
+	if(!hand) {
+		return;
+		//This will break from the current function
+		//and go to the previous function
+		//being the previous iteration of hand
+	}
+
+	free_hand(hand->left);
+	free_hand(hand->right);
+	free(hand);
+
+}
